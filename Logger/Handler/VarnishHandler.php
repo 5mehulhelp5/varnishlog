a5cd59737e54816f108b3e6874a4a4ea5d6b6c28
Kamlesh Bar <barkamlesh@gmail.com>
2025-07-26 18:31:33 +1000

    Logger added

---
+++ Logger/Handler/VarnishHandler.php
@@ -0,0 +1,9 @@
+<?php
+namespace Kamlesh\VarnishLog\Logger\Handler;
+
+use Monolog\Handler\StreamHandler;
+
+class VarnishHandler extends StreamHandler
+{
+    // You can add custom logic here if needed
+}
